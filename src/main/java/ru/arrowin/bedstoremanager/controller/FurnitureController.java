b0b0b32a679241a7f2adf7f8bd08ff6c20aabab0
паша <pashka_ertaev@mail.ru>
2023-02-12 16:45:09 +0300

    Добавлен метод подсчета сделки за кровати

--- src/main/java/ru/arrowin/bedstoremanager/controller/FurnitureController.java
+++ src/main/java/ru/arrowin/bedstoremanager/controller/FurnitureController.java
@@ -3,31 +3,31 @@ package ru.arrowin.bedstoremanager.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.arrowin.bedstoremanager.models.Furniture;
-import ru.arrowin.bedstoremanager.services.FurnitureService;
+import ru.arrowin.bedstoremanager.models.Bed;
+import ru.arrowin.bedstoremanager.services.BedService;
 
 @RestController
 @RequestMapping("/furniture")
 @Tag(name = "Сделка", description = "Подсчет оплаты за выполненную работу")
 public class FurnitureController {
 
-    private final FurnitureService furnitureService;
+    private final BedService bedService;
 
 
-    public FurnitureController(FurnitureService furnitureService) {
-        this.furnitureService = furnitureService;
+    public FurnitureController(BedService bedService) {
+        this.bedService = bedService;
     }
 
     @PostMapping("create")
-    public ResponseEntity<Furniture> addFurniture(@RequestBody Furniture furniture) {
-        furnitureService.add(furniture);
-        return ResponseEntity.ok(furniture);
+    public ResponseEntity<Bed> addBed(@RequestBody Bed bed) {
+        bedService.add(bed);
+        return ResponseEntity.ok(bed);
     }
 
     @GetMapping("getsum")
-    public ResponseEntity<Double> getAllSalaryInDay(@RequestParam double cost){
-        furnitureService.allCostInDay();
-        return ResponseEntity.ok(cost);
+    public ResponseEntity<Double> getAllSalaryInDay(){
+        double sum = bedService.allCostInDay();
+        return ResponseEntity.ok(sum);
     }
 
 }
--- src/main/java/ru/arrowin/bedstoremanager/repository/FurnitureRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.arrowin.bedstoremanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.arrowin.bedstoremanager.models.Furniture;
-
-public interface FurnitureRepository extends JpaRepository<Furniture,Double>{
-}
