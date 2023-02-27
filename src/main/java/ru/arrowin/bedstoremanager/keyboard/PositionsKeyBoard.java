a2eed521c715f51831cda4fd168cfc2d84b0ecd5
Arrowin24 <igor_rudenko.ru@mail.ru>
2023-02-27 23:39:32 +0700

    Изменил тип position в Worker.java на ENUM. Добавил в CreateWorkerStep.java шаг с выбором должности и подтверждением пароля.

---
+++ src/main/java/ru/arrowin/bedstoremanager/keyboard/PositionsKeyBoard.java
@@ -0,0 +1,22 @@
+package ru.arrowin.bedstoremanager.keyboard;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import ru.arrowin.bedstoremanager.models.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PositionsKeyBoard implements KeyBoard {
+    @Override
+    public InlineKeyboardMarkup getKeyBoard() {
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        for (Position position : Position.values()) {
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(position.name());
+            button.setCallbackData(position.name());
+            keyboard.add(createButtonsLine(button));
+        }
+        return new InlineKeyboardMarkup(keyboard);
+    }
+}
--- src/main/java/ru/arrowin/bedstoremanager/step/StepName.java
+++ src/main/java/ru/arrowin/bedstoremanager/step/StepName.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum StepName {
+    ERROR(-1),
     ZERO(0),
     ONE(1),
     TWO(2),
     THREE(3),
-    FOUR(4)
+    FOUR(4),
+    FIVE(5)
     ;
 
     private final int stepNum;
