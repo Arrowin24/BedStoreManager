c7579c1101afa92d0838d7f3849fe8b8b8e446bd
Arrowin24 <igor_rudenko.ru@mail.ru>
2023-02-28 20:45:15 +0700

    Добавил запрос в JPA Repository CreatedBedsRepository.java.

--- src/main/java/ru/arrowin/bedstoremanager/repository/CreatedBedsRepository.java
+++ src/main/java/ru/arrowin/bedstoremanager/repository/CreatedBedsRepository.java
@@ -1,7 +1,15 @@
 package ru.arrowin.bedstoremanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.arrowin.bedstoremanager.models.CreatedBed;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CreatedBedsRepository extends JpaRepository<CreatedBed, Integer> {
+
+    @Query(value = "SELECT * FROM created_beds WHERE user_id=?1 and date=?2",
+           nativeQuery = true)
+    List<CreatedBed> findBedsByWorkerIdAndDate(Long userId, LocalDate date);
 }
